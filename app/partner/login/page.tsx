47e7cfb92d854b1f1da709a0fd2faa865446a117
Orizu <michael@limpiar.online>
2025-06-18 15:52:31 +0100

    new

--- app/partner/login/page.tsx
+++ app/partner/login/page.tsx
@@ -1,12 +1,10 @@
 "use client";
-import { Suspense } from "react";
 import { LoginForm } from "@/cleaningBusiness/component/auth/login-form";
 
 export const dynamic = "force-dynamic";
 export default function LoginPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-4">
-         <Suspense fallback={<div>Loading...</div>}></Suspense>
       <LoginForm />
     </div>
   );
